b8c01dff5db252ac1309b234a2b695aff4242cb6
Niels Koster <n.koster@portavita.eu>
2021-07-18 15:16:10 +0200

    Use config.json

--- bulkmailer.js
+++ bulkmailer.js
@@ -6,25 +6,17 @@ const EmailList = require('./EmailList.json')
 let html = fs.readFileSync('./message/index.html', 'utf8')
 
 const attachments = require('./Attachments')
+attachments.forEach(img => 
+    html = html.replace(`images/${img.filename}`, `cid:${img.filename}`))
 
-attachments.forEach(img => html = html.replace(`images/${img.filename}`, `cid:${img.filename}`))
+const { configTransporter } = require('./config.json')
+const transporter = nodemailer.createTransport(configTransporter)
 
-const transporter = nodemailer.createTransport({
-    port: 25,
-    host: 'localhost',
-    tls: {
-      rejectUnauthorized: false
-    }
-})
-
+const { from, subject, text } = require('./config.json').configHeader
 EmailList.forEach(({name, email}) => {
     let message = {
-        from: 'noreply@drone-existence.com',
-        to: `${name}<${email}>`,
-        subject: 'UAZ Bukhanka',
-        text: 'UAZ Bukhanka',
-        attachments,
-        html
+        from, to: `${name}<${email}>`,
+        subject, text, attachments, html
     }
     transporter.sendMail(message, (error, info) => {
         if (error) {
